acf30daf773e37cea05d357ca888731e3494c4a3
Yoonsu Kang <kangys0725@naver.com>
2019-05-24 11:17:57 +0900

    TeaBagMaker 콤보박스를 이용한 디자인 구현

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -29,12 +29,79 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ChoiceTea = new System.Windows.Forms.ComboBox();
+            this.MakeTea = new System.Windows.Forms.Button();
+            this.TeaTimer = new System.Windows.Forms.Timer(this.components);
+            this.TCountDown = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans CJK KR Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "티를 선택하세요.";
+            // 
+            // ChoiceTea
+            // 
+            this.ChoiceTea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ChoiceTea.FormattingEnabled = true;
+            this.ChoiceTea.Location = new System.Drawing.Point(18, 57);
+            this.ChoiceTea.Name = "ChoiceTea";
+            this.ChoiceTea.Size = new System.Drawing.Size(246, 20);
+            this.ChoiceTea.TabIndex = 1;
+            // 
+            // MakeTea
+            // 
+            this.MakeTea.Font = new System.Drawing.Font("Noto Sans CJK KR Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.MakeTea.Location = new System.Drawing.Point(18, 83);
+            this.MakeTea.Name = "MakeTea";
+            this.MakeTea.Size = new System.Drawing.Size(246, 65);
+            this.MakeTea.TabIndex = 2;
+            this.MakeTea.Text = "담그기!";
+            this.MakeTea.UseVisualStyleBackColor = true;
+            this.MakeTea.Click += new System.EventHandler(this.MakeTea_Click);
+            // 
+            // TeaTimer
+            // 
+            this.TeaTimer.Interval = 1000;
+            // 
+            // TCountDown
+            // 
+            this.TCountDown.Font = new System.Drawing.Font("Noto Sans CJK KR Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TCountDown.Location = new System.Drawing.Point(18, 155);
+            this.TCountDown.Name = "TCountDown";
+            this.TCountDown.Size = new System.Drawing.Size(246, 47);
+            this.TCountDown.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(283, 209);
+            this.Controls.Add(this.TCountDown);
+            this.Controls.Add(this.MakeTea);
+            this.Controls.Add(this.ChoiceTea);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ChoiceTea;
+        private System.Windows.Forms.Button MakeTea;
+        private System.Windows.Forms.Timer TeaTimer;
+        private System.Windows.Forms.TextBox TCountDown;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -7,13 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        //콤보박스 데이터 설정
+        string[] TeaList = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        int CountOrgNum = 0; //초기 카운터
+        string Choice = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.TeaList.Length; i++)
+            {
+                this.ChoiceTea.Items.Add(TeaList[i]);
+            }
+            
+        }
+
+        private void MakeTea_Click(object sender, EventArgs e)
+        {
+            Choice = TeaList.ToString();
+
+            if (Choice == "홍차")
+            {
+                MessageBox.Show("홍차");
+            }
         }
     }
 }
